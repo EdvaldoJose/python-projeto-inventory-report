52f7c89e899ebf06bac963f256b3f0a1a927b817
Edvaldo José <edvaldojose003@gmail.com>
2023-07-16 18:47:13 -0300

    fleat: 3. Crie a Interface Importer

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,16 @@
+from abc import ABC, abstractmethod
 from typing import Dict, Type
 
+from inventory_report.product import Product
 
-class Importer:
-    pass
+
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        pass
 
 
 class JsonImporter:
