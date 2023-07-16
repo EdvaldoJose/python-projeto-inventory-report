f3db092948d26276e1fe9de04d7adb9253253163
Edvaldo José <edvaldojose003@gmail.com>
2023-07-16 20:17:29 -0300

    feat: 6. Crie o protocolo Report

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,11 @@
-class Report:
-    pass
+from typing import Protocol
+
+from inventory_report.inventory import Inventory
+
+
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    def generate(self) -> str:
+        pass
