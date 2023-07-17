5e3168e656ddd2fbb40d1ceac354673f168b08ed
Edvaldo José <edvaldojose003@gmail.com>
2023-07-17 07:42:20 -0300

    feat: 7. Crie o relatório SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,36 @@
-class SimpleReport:
-    pass
+from datetime import date
+from typing import List
+
+from inventory_report.inventory import Inventory
+from inventory_report.product import Product
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self.inventories: List[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        for inventory in self.inventories:
+            sorted_man_dte = sorted(
+                inventory.data, key=lambda prod: prod.manufacturing_date
+            )
+
+        def expiration_date_key(product: Product) -> str:
+            if product.expiration_date >= str(date.today()):
+                return product.expiration_date
+            else:
+                return str(date.max)
+
+        sorted_expirat_dte = sorted(
+            inventory.data, key=expiration_date_key)
+
+        company = max(inventory.data, key=lambda prod: prod.company_name)
+        return (
+            f"Oldest manufacturing date:{sorted_man_dte[0].manufacturing_date}"
+            f"Closest expiration date: {sorted_expirat_dte[0].expiration_date}"
+            f"Company with the largest inventory: {company.company_name}"
+        )
