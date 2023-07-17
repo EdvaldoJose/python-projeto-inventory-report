283b2b79d25316794470d5ea006f767c1c82aba6
Edvaldo José <edvaldojose003@gmail.com>
2023-07-17 09:13:38 -0300

    feat: 9. Crie o relatório CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
-class CompleteReport:
-    pass
+from collections import Counter
+
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        companies = ""
+        for inventory in self.inventories:
+            stock_company = Counter(
+                product.company_name for product in inventory.data
+            )
+        for company, quantity in stock_company.items():
+            companies += (f"{company}:{quantity}")
+
+        return (super().generate() + "Stocked products by company:\n" +
+                companies)
